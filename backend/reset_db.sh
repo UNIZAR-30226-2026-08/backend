a746d016333afa9d6823eabd5d29c33a439a30f4
Jaime A. <jalconchel03@gmail.com>
2026-01-31 22:26:30 +0100

    basic user model working

---
+++ backend/reset_db.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+rm -rf db.sqlite3
+for f in magnate
+do
+    rm -rf $f/migrations
+done
+
+for f in magnate
+do
+    python manage.py makemigrations $f
+done
+
+python manage.py migrate
+python manage.py runserver
+
+

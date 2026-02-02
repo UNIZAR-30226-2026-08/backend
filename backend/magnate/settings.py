b36356c6f8bda329a8818e67f6954c9cec9ac409
845977 <845977@unizar.es>
2026-02-02 16:22:18 +0100

    Fixes #9

--- backend/magnate/settings.py
+++ backend/magnate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from .private import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(-@^*pd=dky4jrqzmr(d%-j35rn#joio6op6x(4cr%7vccgqej'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

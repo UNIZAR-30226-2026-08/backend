b80c5be66061cb40900010f7c1b44525ef8aeb90
hugonaudin <hugonaudin03@gmail.com>
2026-02-03 14:53:25 +0100

    Fixes #11

--- backend/magnate/consumers.py
+++ backend/magnate/consumers.py
@@ -4,24 +4,38 @@ from .models import QueueMetadata, QueuePosition
 
 # TODO: check how to implement DB access form async context -> database_sync_to_async in doc
 
-class QueueConsumer(AsyncWebsocketConsumer):
+class PublicQueueConsumer(AsyncWebsocketConsumer):
     
     async def connect(self):
-        # Triggered when user opens the matchmaking page. Accept connection & auth.
+        # Triggered when user opens the matchmaking page or "Join Game". Accept connection & auth.
         pass
 
     async def disconnect(self, close_code):
-        # Triggered when user closes tab or finds a match. Remove from DB 
+        # Triggered when user closes tab or finds a match -> manage differences. Remove from DB 
         pass
 
     async def receive(self, text_data):
-        # Triggered when client sends 'join_queue' command or polls for status.
+        # Not used for now
+        pass
+
+
+class PrivateRoomConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        # Triggered when user opens a new private room or joins an existing one.
+        pass
+
+    async def disconnect(self, close_code):
+        # Triggered when user leaves the private room lobby.
+        pass
+    
+    async def receive(self, text_data):
+        # Chat messages or 'start_game' command /'ready' status updates.
         pass
 
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Triggered when user joins a specific match ID -> add to Redis room group.
+        # Triggered when user joins a specific match ID (game really begins) -> add to Redis room group.
         pass
 
     async def disconnect(self, close_code):
@@ -30,4 +44,5 @@ class GameConsumer(AsyncWebsocketConsumer):
     
     async def receive(self, text_data):
         # Triggered when user sends a move -> broadcast to room group.
+        # Also manages game over conditions triggering disconnects.
         pass

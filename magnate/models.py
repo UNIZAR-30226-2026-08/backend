b925290203f216de462b75a3ae59b0cca9d318c6
hugonaudin <hugonaudin03@gmail.com>
2026-04-24 00:02:07 +0200

    Fix end gfame

--- magnate/models.py
+++ magnate/models.py
@@ -1033,6 +1033,7 @@ class PlayerGameStatistic(models.Model):
     num_paid_rents = models.PositiveIntegerField(default=0)
     num_trades = models.PositiveIntegerField(default=0)
     num_mortgages = models.PositiveIntegerField(default=0)
+    end_game = models.PositiveBigIntegerField(default=0)
     
     class Meta:
         # 1 player and game for each stats
@@ -1090,6 +1091,7 @@ class BonusCategory(models.Model):
         num_paid_rents    = 'num_paid_rents'
         num_trades        = 'num_trades'
         num_mortgages     = 'num_mortgages'
+        end_game = 'end_game'
 
     stat_field = models.CharField(choices=StatField, max_length=30, unique=True)
     bonus_amount = models.PositiveIntegerField(default=200)

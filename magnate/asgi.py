cc25625653a30fff8621722db8f5f7621297ec77
845977 <845977@unizar.es>
2026-05-09 01:57:11 +0200

    asdhjasdkla

--- magnate/asgi.py
+++ magnate/asgi.py
@@ -8,20 +8,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import magnate.routing 
-from magnate.middleware import JWTAuthMiddleware
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magnate.settings')
 
+# Must call setup() before any import that touches models/apps
+import django
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import magnate.routing
+from magnate.middleware import JWTAuthMiddleware
+
 application = ProtocolTypeRouter({
-    # Normal HTTP requests are handled by Django
     "http": get_asgi_application(),
-    
-    # Websocket requests are handled by Channels
     "websocket": JWTAuthMiddleware(
         URLRouter(
             magnate.routing.websocket_urlpatterns

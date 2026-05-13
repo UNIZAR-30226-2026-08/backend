0ca33e43e69ed2086984e33ba7ec8cc14aa50e7a
nin-linux <nintriks@gmail.com>
2026-05-13 13:01:16 +0200

    force short game for testing

--- magnate/models.py
+++ magnate/models.py
@@ -538,7 +538,7 @@ class Game(models.Model):
     current_turn = models.PositiveIntegerField(default=1)
 
     current_round = models.PositiveIntegerField(default=1)
-    max_rounds = models.PositiveIntegerField(default=60) # 0 for unlimited
+    max_rounds = models.PositiveIntegerField(default=2) # 0 for unlimited
 
 
 class Auction(models.Model):

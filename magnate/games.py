87e16daf28870962dc2d18be4695986dca77a786
hugonaudin <hugonaudin03@gmail.com>
2026-05-14 21:53:52 +0200

    fixing ppk and username conflict

--- magnate/games.py
+++ magnate/games.py
@@ -1302,12 +1302,12 @@ class GameManager:
                     bonus_per_player[key] = bonus_per_player.get(key, 0) + bonus_amount
 
             pk_to_username = {str(stat.user.pk): stat.user.username for stat in all_participants}
-            final_money_dict = {pk: final_money_dict[username] for pk, username in pk_to_username.items()}
+            final_money_dict_pk = {pk: final_money_dict[username] for pk, username in pk_to_username.items()}
 
             # last_money = dinero final sin contar los bonuses
             last_money_dict = {
                 pk: amount - bonus_per_player.get(pk, 0)
-                for pk, amount in final_money_dict.items()
+                for pk, amount in final_money_dict_pk.items()
             }
             response.last_money = last_money_dict
             
